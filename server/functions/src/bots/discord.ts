00cba5ca61430729fd59662931277794df727c4b
tingfei <1228959039@qq.com>
2022-05-24 23:30:19 +0800

    add discord bot

--- server/functions/src/bots/discord.ts
+++ server/functions/src/bots/discord.ts
@@ -1,16 +1,42 @@
 import { Client, Intents } from "discord.js";
 import { token } from "../config/discordconfig.json";
-// import * as functions from "firebase-functions";
+import { Message } from "../utils/type";
+import { db } from "../config/firebase";
 
-const client = new Client({ intents : [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents : [Intents.FLAGS.GUILDS] })
+
+async function get_users() {
+    const users = await (await db.collection("users").get()).docs;
+    return users;
+};
+
+const users = get_users();
+
+let msgs: Message[] = [];
 
 client.once('ready', () => {
     console.log('Ready!');
     client.channels.cache.forEach(channel => {
         if (channel.type === "GUILD_TEXT") {
             channel.messages.fetch().then(messages => {
-                messages.forEach(msg => {
-                    console.log(msg.content);
+                messages.forEach(msg =>  {
+                    const username = `${msg.author.username}#${msg.author.discriminator}`;
+                    console.log(username);
+                    users.then(us => {
+                        us.forEach(u => {
+                            const data = u.data();
+                            if(username === data.username) {
+                                console.log(username);
+                                console.log(data);
+                            }
+                        })
+                    });
+                    
+                    // let m = {
+                    //     "avatar": msg.author.avatarURL(),
+                    //     "username": `${msg.author.username} ${msg.author.discriminator}`,
+                        
+                    // };
                 });
             });
         }
